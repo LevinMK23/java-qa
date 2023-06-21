4922da0e2ad9f89525309a4d83305083c4d699a5
Shandra An <shandra.anastasya@yandex.ru>
2023-06-21 11:17:39 +0300

    first commit

--- src/test/java/ru/yandex/HomeWorkTests.java
+++ src/test/java/ru/yandex/HomeWorkTests.java
@@ -1,11 +1,54 @@
 package ru.yandex;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeWorkTests {
 
     @Test
-    void test() {
-        HomeWork.reverse(12);
+    void testIsReversed() {
+        int value = 2345;
+        int actual = HomeWork.reverse(value);
+        int expected = 5432;
+        Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void testIsFourSymbols(){
+        int value = 9871;
+        int numDigits = String.valueOf(value).length();
+        if (numDigits == 4) {
+            int actual = HomeWork.reverse(value);
+            int expected = 1789;
+            Assertions.assertEquals(expected, actual);
+        }
+        {
+            System.out.println("Введенное число содержит не подходящее количество символов");
+        }
+    }
+
+    @Test
+    void testCountDifference(){
+        int number = 5511;
+        int expected = 44;
+        int actual = HomeWork.difference(number);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCountDifferenceBoard1() {
+        int number = 10;
+        int expected = -10;
+        int actual = HomeWork.difference(number);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testCountDifferenceBoard2() {
+        int number = 9999;
+        int expected = 0;
+        int actual = HomeWork.difference(number);
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
